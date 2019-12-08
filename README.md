5608430b31971d290df9b33b826db2b8926dc34f
Ben <quillaja@hotmail.com>
2019-12-07 23:12:15 -0800

    works

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # unusable
 the **UNU**sable **S**t**A**ck **B**ased **L**anguag**E**
 
+A toy stack based language written in Go.
+
 # stack
 64 bit signed integers
 'infinite' depth
@@ -10,7 +12,7 @@ single file
 read line by line
 
 line is:
-`KEYWORD [args...]`
+`KEYWORD [arg]`
 
 # keywords
 arg types:
@@ -33,12 +35,13 @@ arg types:
 | mul | | multiplies the top 2 elements of the stack and puts the result on the stack |
 | div | | divides the 2nd-from-top element by the top element and puts the result on the stack |
 | mod | | modulus the 2nd-from-top element by the top element and puts the result on the stack |
-| pow | | |
+| pow | | raises 2nd-from-top element by the top element and puts the result on the stack |
 | | | |
 | eq | | compares the top 2 elements. puts `1` on the stack if equal, `0` if not equal |
 | neq | | compares the top 2 elements. `1` if not equal, `0` if equal |
-| gt | | compares the top 2 ele|
+| gt | | compares the 2nd-from-top element with top element.|
 | gte | | |
+| lt | | |
 | lte | | |
 | not | | inverts the 'truth' of the top element. Non-zero (true) becomes `0` (false). `0` (false) becomes a random non-zero integer (true). |
 | | | |
--- main.go
+++ main.go
@@ -3,38 +3,54 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
+	"math"
+	"math/rand"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
-var lineParseRe *regexp.Regexp
+var (
+	lineParseRe *regexp.Regexp
+)
 
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	lineParseRe = regexp.MustCompile(`^(\S+)\s*((?:\S*)|(?:\".+\"))\s*(?:\s#.*)?$`)
 }
 
 func main() {
-	const prog = `
-push	-5
-push	4	# comment
-add
 
-push A
+	flag.Parse()
+	filepath := flag.Arg(0)
 
-# a comment
-#another comment
-println	I
-print	N
-read	"Is this 9? "
-`
+	if filepath == "" {
+		fmt.Println("no program file specified")
+		os.Exit(1)
+	}
 
-	fmt.Printf("program:\n%s\n\n", prog)
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-	s := bufio.NewScanner(strings.NewReader(prog))
+	s := bufio.NewScanner(file)
 	lineNumber := 0
 	stack := newStack()
+
+	defer func() {
+		perr := recover()
+		if perr, ok := perr.(error); ok {
+			fmt.Printf("error on line %d: %s\n", lineNumber, perr)
+			os.Exit(1)
+		}
+	}()
 
 	for s.Scan() {
 		line := strings.TrimSpace(s.Text())
@@ -45,50 +61,91 @@ read	"Is this 9? "
 
 		cmd, arg, err := splitCmdArg(line)
 		if err != nil {
-			fmt.Printf("error on line %d: %s\n", lineNumber, err)
-			break
+			panic(err)
 		}
 
-		fmt.Printf("cmd: %s\targ: %s\n", cmd, arg)
+		// fmt.Printf("cmd: %s\targ: %s\n", cmd, arg)
 
 		switch cmd {
 		case "push":
-			n, err := strconv.Atoi(arg)
-			if err != nil {
-				n = int(arg[0])
-			}
-			stack.push(int64(n))
+			stack.push(parseInt(arg))
 		case "pop":
 			stack.pop()
 		case "dup":
-			t := stack.pop()
-			stack.push(t)
-			stack.push(t)
+			a := stack.pop()
+			stack.push(a)
+			stack.push(a)
 
 		case "add":
-			a, b := stack.pop(), stack.pop()
-			stack.push(a + b)
+			a, b := stack.pop2()
+			stack.push(b + a)
 		case "sub":
+			a, b := stack.pop2()
+			stack.push(b - a)
 		case "mul":
+			a, b := stack.pop2()
+			stack.push(b * a)
 		case "div":
+			a, b := stack.pop2()
+			stack.push(b / a)
 		case "mod":
+			a, b := stack.pop2()
+			stack.push(b % a)
 		case "pow":
+			a, b := stack.pop2()
+			stack.push(int64(math.Pow(float64(b), float64(a))))
 
 		case "eq":
+			a, b := stack.pop2()
+			stack.pushBool(b == a)
 		case "neq":
+			a, b := stack.pop2()
+			stack.pushBool(b != a)
+
 		case "gt":
+			a, b := stack.pop2()
+			stack.pushBool(b > a)
+
 		case "gte":
+			a, b := stack.pop2()
+			stack.pushBool(b >= a)
+
 		case "lt":
+			a, b := stack.pop2()
+			stack.pushBool(b < a)
+
 		case "lte":
+			a, b := stack.pop2()
+			stack.pushBool(b <= a)
+
 		case "not":
+			a := stack.pop()
+			if a == 0 {
+				stack.push(rand.Int63())
+			} else {
+				stack.push(0)
+			}
 
 		case "print":
+			if arg == "C" {
+				fmt.Print(string(stack.pop()))
+			} else {
+				fmt.Print(stack.pop())
+			}
 		case "println":
+			if arg == "C" {
+				fmt.Println(string(stack.pop()))
+			} else {
+				fmt.Println(stack.pop())
+			}
 		case "read":
+			fmt.Print(strings.Trim(arg, `"`))
+			var in string
+			fmt.Scanln(&in)
+			stack.push(parseInt(in))
 
 		default:
-			fmt.Printf("error on line %d: not a command: %s\n", lineNumber, cmd)
-			break
+			panic(fmt.Errorf("not a command: %s", cmd))
 		}
 	}
 
@@ -102,16 +159,18 @@ func splitCmdArg(line string) (cmd, arg string, err error) {
 		return "", "", errors.New("syntax error")
 	}
 	return matches[1], matches[2], nil
-	// parts := strings.SplitN(line, " ", 2)
-	// cmd = parts[0]
-	// if len(parts) > 1 {
-	// 	rest = parts[1]
-	// }
-	// return
 }
 
 func isComment(line string) bool {
 	return len(line) > 0 && line[0] == '#'
+}
+
+func parseInt(arg string) int64 {
+	n, err := strconv.Atoi(arg)
+	if err != nil {
+		n = int(arg[0])
+	}
+	return int64(n)
 }
 
 type stack []int64
@@ -121,14 +180,33 @@ func newStack() *stack {
 	return &s
 }
 
+func (s *stack) length() int64 {
+	return int64(len(*s))
+}
+
 func (s *stack) push(i int64) {
-	fmt.Printf("\tpushed %d\n", i)
+	// fmt.Printf("\tpushed %d\n", i)
 	*s = append(*s, i)
 }
 
+func (s *stack) pushBool(b bool) {
+	if b {
+		s.push(1)
+		return
+	}
+	s.push(0)
+}
+
 func (s *stack) pop() int64 {
+	if s.length() < 1 {
+		panic(fmt.Errorf("stack underflow"))
+	}
 	top := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
-	fmt.Printf("\tpopped %d\n", top)
+	// fmt.Printf("\tpopped %d\n", top)
 	return top
+}
+
+func (s *stack) pop2() (int64, int64) {
+	return s.pop(), s.pop()
 }
